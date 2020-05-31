0668958ab6014871075bc62c30561291edb4a2f9
Dr. Karsten Schmidt <karsten.schmidt@audi.de>
2020-05-31 21:31:32 +0200

    Update

--- Uart.h
+++ Uart.h
@@ -2,7 +2,7 @@
  * Copyright (c) 2006, 2007, 2008 by Hartmut Birr
  *
  * This program is free software; you can redistribute it and/or
- * mmodify it under the terms of the GNU General Public License
+ * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
