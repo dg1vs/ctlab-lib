3a27d224445c84db9ed2af1c84b563b04214f553
Karsten Schmidt <15368701+dg1vs@users.noreply.github.com>
2022-01-06 21:48:13 +0100

    Update Lcd.c

--- Lcd.c
+++ Lcd.c
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2007 by Hartmut Birr
+ * Copyright (c) 2007, 2008 by Hartmut Birr
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
+ * mmodify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
@@ -17,15 +17,16 @@
  *
  */
 
+
 #include <inttypes.h>
 #include <avr/pgmspace.h>
 #include <util/delay.h>
-
 #include <string.h>
 
-#include "panel.h"
 #include "I2CRegister.h"
 #include "Lcd.h"
+#include "Config.h"
+#include "Config_LcdChar.h"
 
 #define NDEBUG
 #include "debug.h"
@@ -35,40 +36,46 @@
 // P1.1 -> R/W
 // P1.2 -> RS
 
+
 #ifdef COMPILE_WITH_DISPLAY204
-static char DisplayData[80];
-const PROGMEM char ucWhites[] = "                    ";    /* 20 spaces */
+	static char DisplayData[80];
+	const PROGMEM char ucWhites[] = "                    ";    /* 20 spaces */
 #else
-static char DisplayData[16];
-const PROGMEM char ucWhites[] = "        ";                /* 8 spaces */
+	static char DisplayData[16];
+	const PROGMEM char ucWhites[] = "        ";                /* 8 spaces */
 #endif
 
-static void LCDWriteCmd(uint8_t value)
+void __Lcd_WriteCmd(uint8_t value)
 {
     uint8_t data[2];
 
     data[0] = value;
 
-    data[1] = 1;                        // RS=0, R/W=0, E=1
+    data[1] = 0;                        // RS=0, R/W=0, E=0
     I2CRegister_Write(LCD_PCA9555D_ADDRESS, 2, 2, data);
+
+    data[1] = 1;                        // RS=0, R/W=0, E=1
+    I2CRegister_Write(LCD_PCA9555D_ADDRESS, 3, 1, data + 1);
 
     data[1] = 0;                        // RS=0, R/W=0, E=0
     I2CRegister_Write(LCD_PCA9555D_ADDRESS, 3, 1, data + 1);
 }
 
-static void LCDWriteData(uint8_t value)
+void __Lcd_WriteData(uint8_t value)
 {
     uint8_t data[2];
 
     data[0] = value;
 
-    data[1] = 0x05;                     // RS=1, R/W=0, E=1
+    data[1] = 0x04;                     // RS=1, R/W=0, E=0
     I2CRegister_Write(LCD_PCA9555D_ADDRESS, 2, 2, data);
+
+    data[1] = 0x05;                     // RS=1, R/W=0, E=1
+    I2CRegister_Write(LCD_PCA9555D_ADDRESS, 3, 1, data + 1);
 
     data[1] = 0x04;                     // RS=1, R/W=0, E=0
     I2CRegister_Write(LCD_PCA9555D_ADDRESS, 3, 1, data + 1);
 }
-
 
 uint8_t Lcd_Init(void)
 {
@@ -76,70 +83,65 @@ uint8_t Lcd_Init(void)
     uint8_t i;
 
 
-    /* config register see schematics */
+    // config register
     data[0] = 0x00;                     // P0 is output
     data[1] = 0xf8;                     // P1.0-2 is output, P1.3-7 is input
     if (I2CRegister_Write(LCD_PCA9555D_ADDRESS, 6, 2, data))
     {
-        // TODO
-		// LOG_ERROR(PSTR("%s: Couldn't detect a device at address LCD_PCA9555D_ADDRESS \n"), __FUNCTION__);
+        DPRINT(PSTR("%s: Couldn't detect a device at address LCD_PCA9555D_ADDRESS \n"), __FUNCTION__);
         return 0;
     }
 
     // polarity inversion register
     data[0] = 0x00;
-    data[1] = 0xf8;                     /* invert the buttons; now 4 buttons */
+    data[1] = 0x38;                     // invert the buttons
     I2CRegister_Write(LCD_PCA9555D_ADDRESS, 4, 2, data);
+
 
     _delay_ms(30);
 
 #ifdef COMPILE_WITH_DISPLAY204
-    LCDWriteCmd(0x34);                  // System set
-    LCDWriteCmd(0x09);                  //
-    LCDWriteCmd(0x30);
-    LCDWriteCmd(0x0c);                  // display on
-    LCDWriteCmd(0x01);                  // clear Display
+    __Lcd_WriteCmd(0x34);                  // System set
+    __Lcd_WriteCmd(0x09);                  //
+    __Lcd_WriteCmd(0x30);
+    __Lcd_WriteCmd(0x0c);                  // display on
+    __Lcd_WriteCmd(0x01);                  // clear Display
     _delay_ms(2);
-
-    LCDWriteCmd(0x06);                  // Entry Mode set
+    __Lcd_WriteCmd(0x06);                  // Entry Mode set
 #else
-    LCDWriteCmd(0x38);                  // System set
-
-    LCDWriteCmd(0x0c);                  // display on
-    LCDWriteCmd(0x01);                  // clear Display
+    __Lcd_WriteCmd(0x38);                  // System set
+    __Lcd_WriteCmd(0x0c);                  // display on
+    __Lcd_WriteCmd(0x01);                  // clear Display
     _delay_ms(2);
-    LCDWriteCmd(0x06);                  // Entry Mode set
+    __Lcd_WriteCmd(0x06);                  // Entry Mode set
 #endif
 
-    LCDWriteCmd(LCD_PCA9555D_ADDRESS);
-    for (i = 0; i < sizeof(cursor); i++)
+    __Lcd_WriteCmd(0x40);
+	// from Config_Char.h and Config_Char.c
+    for (i = 0; i < sizeof(LCD_ExtraCharacter); i++)
     {
-        LCDWriteData(pgm_read_byte(&cursor[i]));
+        __Lcd_WriteData(pgm_read_byte(&LCD_ExtraCharacter[i]));
     }
-
     memset(DisplayData, ' ', sizeof(DisplayData));
-
     return 1;
 }
 
-static void LCDInternalWrite(uint8_t x, uint8_t y, uint8_t len, const char* data, uint8_t prog)
+static void __Lcd_InternalWrite(uint8_t x, uint8_t y, uint8_t len, const char* data, uint8_t prog)
 {
     uint8_t addr;
     uint8_t valid;
     char ch;
     char* ptr;
 
-
     if (x >= COLUMN_MAX || y >= LINE_MAX)
     {
         return;
     }
-
     addr = x;
 
 #ifdef COMPILE_WITH_DISPLAY204
     // TODO check case vs. if else if in asm
-    /* see data-sheet about adress */
+    /* see data-sheet about address */
     if (y == 1)
     {
         addr += 0x20;
@@ -159,7 +161,6 @@ static void LCDInternalWrite(uint8_t x, uint8_t y, uint8_t len, const char* data
     }
 #endif
 
-
     if (x + len > COLUMN_MAX)
     {
         len = COLUMN_MAX - x;
@@ -178,10 +179,10 @@ static void LCDInternalWrite(uint8_t x, uint8_t y, uint8_t len, const char* data
         {
             if (!valid)
             {
-                LCDWriteCmd(0x80 | addr);
+                __Lcd_WriteCmd(0x80 | addr);
                 valid = 1;
             }
-            LCDWriteData(ch);
+            __Lcd_WriteData(ch);
             *ptr = ch;
         }
         data++;
@@ -193,17 +194,17 @@ static void LCDInternalWrite(uint8_t x, uint8_t y, uint8_t len, const char* data
 
 void Lcd_Write(uint8_t x, uint8_t y, uint8_t len, const char* data)
 {
-    LCDInternalWrite(x, y, len, data, 0);
+    __Lcd_InternalWrite(x, y, len, data, 0);
 }
 
 void Lcd_Write_P(uint8_t x, uint8_t y, uint8_t len, const char* data)
 {
-    LCDInternalWrite(x, y, len, (const char*)data, 1);
+    __Lcd_InternalWrite(x, y, len, (const char*)data, 1);
 }
 
 void Lcd_OverWrite_P(uint8_t x, uint8_t y, uint8_t len, const char* data)
 {
-    // clearing leading and trailing whitespaces instead of calling LCDClearLine(y) to save traffic on I2C-Bus
+    //  clearing leading and trailing whitespaces instead of calling LCDClearLine(y) to save traffic on I2C-Bus
     Lcd_Write_P(0, y, x, ucWhites);
     Lcd_Write_P(x+len, y, COLUMN_MAX, ucWhites);
 
